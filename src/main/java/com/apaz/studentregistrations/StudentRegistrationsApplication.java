543d93729791702ee620645bbee58dad81fd0a42
Ariel Gustavo <arielgdapaz@gmail.com>
2024-05-09 20:57:30 -0300

    first commit

---
+++ src/main/java/com/apaz/studentregistrations/StudentRegistrationsApplication.java
@@ -0,0 +1,13 @@
+package com.apaz.studentregistrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistrationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apaz/studentregistrations/StudentRegistrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apaz.studentregistrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
